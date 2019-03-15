f89d3e0e68a482106c857b61c3a741b9a3e9b0dd
cerise <cerise.andres@yahoo.fr>
2019-03-15 11:15:45 +0100

    commit@cerise

--- resources/views/aurevoir.blade.php
+++ resources/views/aurevoir.blade.php
@@ -68,7 +68,7 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Bonjour !
+                    Au revoir !
                 </div>
             </div>
         </div>
